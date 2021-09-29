2853b038e153bae67ac7196f087c23d83c3b3028
jonathan <4jonathanfoster@gmail.com>
2021-09-28 23:46:37 -0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ const Navbar = () => {
           <a className="nav-link" href="#">Services</a>
           </li>
           <li className="nav-item">
-          <a className="nav-link" href="#">Work</a>
+          <a className="nav-link" href="#">Work History</a>
           </li>
           <li className="nav-item">
           <a className="nav-link" href="#">Portfolio</a>
